814f7be3d6b11c9b426212797757cdb3e7d68810
Nhlanhla <nfortunengcobo@gmail.com>
2024-11-03 16:44:58 +0200

    Fisrt component added

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import  IndexPageComponentA from'./IndexPageComponentA';
+
+function App() {
+    const [forecasts, setForecasts] = useState();
+
+    useEffect(() => {
+        //populateWeatherData();
+
+        const handleMouseMove = (e) => {
+            const gradient = `radial-gradient(circle ${500}px  at ${e.clientX}px ${e.clientY}px, #304c64, #132335)`;
+            document.body.style.background = gradient;
+        };
+
+        const handleMouseLeave = () => {
+            document.body.style.background = ''; // Reset background when not hovering
+        };
+
+        document.body.addEventListener('mousemove', handleMouseMove);
+        document.body.addEventListener('mouseleave', handleMouseLeave);
+
+        return () => {
+            document.body.removeEventListener('mousemove', handleMouseMove);
+            document.body.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, []);
+
+
+    return (
+        <div>
+            <IndexPageComponentA/>
+        </div>
+    );
+    
+
+    
+    async function populateWeatherData() {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        setForecasts(data);
+    }
+    
+    
+}
+
+export default App;
---
+++ src/IndexPageComponentA.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class IndexPageComponentA extends React.Component {
+    render() {
+      return(
+      <h2>Nhlanhla Ngcobo</h2>
+      )
+    }
+}
+
+export default IndexPageComponentA;

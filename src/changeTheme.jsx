937490b83bb2644710a395950c65a012e912bcb3
Nhlanhla <nfortunengcobo@gmail.com>
2025-05-16 21:18:03 +0200

    added titles

--- src/changeTheme.jsx
+++ src/changeTheme.jsx
@@ -12,9 +12,9 @@ class ChangeThemeComponent extends React.Component {
         <div>
           <button onClick={toggleTheme} className="theme-toggle-btn">
             {theme === "light" ? (
-              <i className="bi bi-moon-fill" style={{ fontSize: 23 }}></i>
+              <i className="bi bi-moon-fill" style={{ fontSize: 23 }} title="Dark Mode"></i>
             ) : (
-              <i className="bi bi-brightness-high" style={{ fontSize: 23, color: "white" }}></i>
+              <i className="bi bi-brightness-high" style={{ fontSize: 23, color: "white" }} title="Light Mode"></i>
             )}
           </button>
         </div>
